d6eb499cac46795dae9c6eba8e021fbbd9f0395f
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-07 11:34:11 -0500

    Update gem, bump

--- templates/clients/ruby/lib/quill/client.rb
+++ templates/clients/ruby/lib/quill/client.rb
@@ -41,40 +41,42 @@ module Quill
       response
     end
 
-    class Endpoint
-      attr_accessor :name, :api
+    module Endpoint
+      class Base
+        attr_accessor :name, :api
 
-      def initialize api, name, definition
-        @api = api
-        @name = name
-        @attributes = definition['attributes'].keys
-        @readonly_attributes = definition['readonly_attributes'].try(:keys) || []
-      end
+        def initialize api, name, definition
+          @api = api
+          @name = name
+          @attributes = definition['attributes'].keys
+          @readonly_attributes = definition['readonly_attributes'].try(:keys) || []
+        end
 
-      def find id, params = {}
-        check_keys(params)
-        response = api.get([name,id].join('/'), params)
-        Hashie::Mash.new(response.body['object'])
-      end
+        def find id, params = {}
+          check_keys(params)
+          response = api.get([name,id].join('/'), params)
+          Hashie::Mash.new(response.body['object'])
+        end
 
-      def create params
-        check_keys(params)
-        response = api.post(name, params)
-        Hashie::Mash.new(response.body['object'])
-      end
+        def create params
+          check_keys(params)
+          response = api.post(name, params)
+          Hashie::Mash.new(response.body['object'])
+        end
 
-      def update id, params
-        check_keys(params)
-        response = api.put([name,id].join('/'), params)
-        Hashie::Mash.new(response.body['object'])
-      end
+        def update id, params
+          check_keys(params)
+          response = api.put([name,id].join('/'), params)
+          Hashie::Mash.new(response.body['object'])
+        end
 
-      def list
-      end
+        def list
+        end
 
-      def check_keys params
-        invalid_keys = params.stringify_keys.keys - @attributes - @readonly_attributes
-        raise InvalidKeys, "Endpoint ##{name} does not support key(s) #{invalid_keys.join(', ')}" unless invalid_keys.empty?
+        def check_keys params
+          invalid_keys = params.stringify_keys.keys - @attributes - @readonly_attributes
+          raise InvalidKeys, "Endpoint ##{name} does not support key(s) #{invalid_keys.join(', ')}" unless invalid_keys.empty?
+        end
       end
     end
   end
