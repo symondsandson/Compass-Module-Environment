5d630f5a937b044d123f59dabe21940815af4d55
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-14 05:53:12 -0500

    wip for docker client

--- docker-automation/lib/client.js
+++ docker-automation/lib/client.js
@@ -1,23 +1,25 @@
-var http        = require('http')
-  , url         = require('url')
-  , querystring = require('querystring');
+var HTTP        = require('http')
+  , URL         = require('url')
+  , QueryString = require('querystring')
+  , Future      = require('fibers/future')
+  , wait        = Future.wait;
 
 var R_NAME_ALREADY_ASSIGNED = /^Conflict, The name .* is already assigned to ([a-f0-9]{12})\. You have to delete \(or rename\) that container to be able to assign postgres to a container again\.$/;
 
 function path (pathname, params) {
-  var u = url.parse(pathname);
-  u.search = querystring.stringify(params);
-  return url.format(u);
+  var url = URL.parse(pathname);
+  url.search = QueryString.stringify(params);
+  return URL.format(url);
 }
 
-function request (method, pathname, params, body, callback) {
+function requestWithCallback (method, pathname, params, body, callback) {
   var options = {
     socketPath: '/var/run/docker.sock',
     path: path(pathname, params),
     method: method
   };
 
-  var req = http.request(options, function (res) {
+  var req = HTTP.request(options, function (res) {
     res.bodyString = '';
     res.setEncoding('utf8');
 
@@ -36,37 +38,34 @@ function request (method, pathname, params, body, callback) {
 
   if (body) req.write(body);
   req.end();
-  return req;
 }
 
-function createContainer (name, options, callback) {
+var request = Future.wrap(requestWithCallback);
+
+function createContainer (name, options) {
   var json = JSON.stringify(options);
-  request('POST', '/containers/create', {name: name}, json, callback);
+  return request('POST', '/containers/create', {name: name}, json).wait();
 }
 
-function findOrCreateContainer (name, options, callback) {
-  createContainer(name, options, function (err, res) {
-    if (err) {
-      debugResponse({message: 'Error in findOrCreateContainer from http.'})
+function findOrCreateContainer (name, options) {
+  var res = createContainer(name, options);
+
+  if (res.statusCode == 409) {
+    var body    = res.bodyString.trim()
+      , matched = body.match(R_NAME_ALREADY_ASSIGNED);
+
+    if (matched) {
+      return { id: matched[1] };
     } else {
-      if (res.statusCode == 409) {
-        var body    = res.bodyString.trim()
-          , matched = body.match(R_NAME_ALREADY_ASSIGNED);
-
-        if (matched) {
-          callback(null, {id: matched[1]});
-        } else {
-          callback({message: 'Failed to parse body string for container ID in findOrCreateContainer.', body: body});
-        }
-      } else {
-        callback(null, res);
-      }
+      throw {message: 'Failed to parse body string for container ID in findOrCreateContainer.', body: body};
     }
-  });
+  } else {
+    return res;
+  }
 }
 
-function runContainer (id, options, callback) {
-  request('POST', '/containers/'+ id +'/start', null, null, callback);
+function runContainer (id, options) {
+  return request('POST', '/containers/'+ id +'/start', null, null).wait();
 }
 
 function debugResponse (err, res) {
@@ -85,9 +84,20 @@ function debugResponse (err, res) {
 
 // ---------------------------------------------------------------------
 
-function launchPostgres (err, res) {
-  debugResponse(err, res);
-  if (err) return;
+Fiber(function() {
+  var res = findOrCreateContainer('postgres', {
+    Hostname: "",
+    User: "",
+    AttachStdout: true,
+    AttachStderr: true,
+    PortSpecs: null,
+    Env: null,
+    Cmd: [
+      "/usr/lib/postgresql/9.3/bin/postgres", "-D", "/var/lib/postgresql/9.3/main", "-c", "config_file=/etc/postgresql/9.3/main/postgresql.conf"
+    ],
+    Dns: null,
+    Image: "ubuntu"
+  });
 
   // docker run --rm -P -v /var/docker-data/etc/postgresql:/etc/postgresql -v /var/docker-data/var/log/postgresql:/var/log/postgresql -v /var/docker-data/var/lib/postgresql:/var/lib/postgresql --name postgres postgres
   runContainer(res.id, {
@@ -97,27 +107,13 @@ function launchPostgres (err, res) {
       '/var/docker-data/var/lib/postgresql:/var/lib/postgresql'
     ],
     PublishAllPorts: true
-  }, debugResponse);
-}
+  });
+}).run();
 
-var res = findOrCreateContainer('postgres', {
-  Hostname: "",
-  User: "",
-  AttachStdout: true,
-  AttachStderr: true,
-  PortSpecs: null,
-  Env: null,
-  Cmd: [
-    "/usr/lib/postgresql/9.3/bin/postgres", "-D", "/var/lib/postgresql/9.3/main", "-c", "config_file=/etc/postgresql/9.3/main/postgresql.conf"
-  ],
-  Dns: null,
-  Image: "ubuntu"
-}, launchPostgres);
-"
-# Create a PostgreSQL role named ``docker`` with ``docker`` as the password and
-# then create a database `docker` owned by the ``docker`` role.
-# Note: here we use ``&&\`` to run commands one after the other - the ``\``
-#       allows the RUN command to span multiple lines.
-RUN /etc/init.d/postgresql start && \
-    psql --command "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';" && \
-    createdb -O docker docker
+// # Create a PostgreSQL role named ``docker`` with ``docker`` as the password and
+// # then create a database `docker` owned by the ``docker`` role.
+// # Note: here we use ``&&\`` to run commands one after the other - the ``\``
+// #       allows the RUN command to span multiple lines.
+// RUN /etc/init.d/postgresql start && \
+//     psql --command "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';" && \
+//     createdb -O docker docker
