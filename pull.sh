3768f1cb575fdb4db09d11654633842a0f43c981
Quinn Shanahan <q.shanahan@gmail.com>
2014-06-07 12:02:11 -0400

    pull

---
+++ pull.sh
@@ -0,0 +1,3 @@
+cd Compass       && (git pull && git submodule update ; true) && cd .. && \
+cd Compass-API   && (git pull && git submodule update ; true) && cd .. && \
+cd Quill-Lessons && (git pull && git submodule update ; true) && cd ..
